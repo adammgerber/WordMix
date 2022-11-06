8f2cabaaddd961adaffb22426a7a5d0ed8b26a61
Adam Milo <amilo@ultralig.ht>
2022-11-06 12:22:21 -0500

    initial layout

--- WordMix/ContentView.swift
+++ WordMix/ContentView.swift
@@ -22,8 +22,29 @@ struct ContentView: View {
         
         //extra validation to come
         
-        usedWords.insert(answer, at: 0)
+        //using insert here instead of append because we want it to appear at the beginning of the list. Otherwise it would show off screen
+        withAnimation{
+            usedWords.insert(answer, at: 0)
+        }
         newWord = ""
+    }
+    
+    func startGame() {
+        //1. Find the URL for start.txt in the app bundle
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt"){
+            //2. Load start.txt into a string
+            if let startWords = try? String(contentsOf: startWordsURL){
+                //3. Split the string up into an array of strings, splitting on line breaks
+                let allWords = startWords.components(separatedBy: "\n")
+                //4. Pick one random word, or use "toybox" as a default word
+                rootWord = allWords.randomElement() ?? "toybox"
+                //5. Everything worked if made it to this point
+                return
+            }
+        }
+        // If we got here, then there was an error. Crash the app and see what happened
+        fatalError("Could not load start.txt from bundle.")
+        
     }
     
     
@@ -32,15 +53,20 @@ struct ContentView: View {
             List{
                 Section{
                     TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
                 }
                 Section {
                     ForEach(usedWords, id: \.self){ word in
-                        Text(word)
+                        HStack{
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
                     }
                 }
             }
             .navigationTitle(rootWord)
             .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
         }
     }
 }
